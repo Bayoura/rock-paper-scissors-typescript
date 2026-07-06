1d3da8d3e9eeff36ead7398a9be0915508560a02
Kristin Seidelmann <Kristin.Seidelmann@gmx.de>
2026-07-06 15:48:32 +0200

    Add game over elements and methods

--- src/ui.ts
+++ src/ui.ts
@@ -1,15 +1,19 @@
-import { GameResult } from "./types";
-
 export class UI {
     private userScoreElement: HTMLSpanElement;
     private computerScoreElement: HTMLSpanElement;
     private roundResultElement: HTMLDivElement;
+    private overlayElement: HTMLDivElement;
+    private winnerTextElement: HTMLHeadingElement;
+    private restartButtonElement: HTMLButtonElement;
 
     constructor() {
         //Get DOM elements
         this.userScoreElement = document.querySelector<HTMLSpanElement>("#user-score")!;
         this.computerScoreElement = document.querySelector<HTMLSpanElement>("#computer-score")!;
         this.roundResultElement = document.querySelector<HTMLDivElement>("#round-result")!;
+        this.overlayElement = document.querySelector<HTMLDivElement>("#game-over-overlay")!;
+        this.winnerTextElement = document.querySelector<HTMLHeadingElement>("#winner-text")!;
+        this.restartButtonElement = document.querySelector<HTMLButtonElement>("#restart-button")!;
     }
 
     renderScores(userScore: number, computerScore: number): void {
@@ -19,5 +23,29 @@ export class UI {
 
     showResult(result: string): void {
         this.roundResultElement.textContent = result;
+    }
+
+    resetUI(): void {
+        this.renderScores(0, 0);
+        this.showResult("");
+    }
+
+    showGameOver(winner: string): void {
+        console.log("GAME OVER");
+        const message: string = winner == "user" ? "You won!" : "Computer won!";
+        this.winnerTextElement.textContent = message;
+        this.showOverlay();
+    }
+
+    private showOverlay(): void {
+        this.overlayElement.classList.add("active");
+    }
+
+    hideOverlay(): void {
+        this.overlayElement.classList.remove("active");
+    }
+
+    onRestart(callback: () => void): void {
+        this.restartButtonElement.addEventListener("click", callback);
     }
 }

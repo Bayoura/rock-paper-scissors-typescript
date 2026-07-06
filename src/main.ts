2f2a667d49258f7bcc0c6472344c3d0399e498f7
Kristin Seidelmann <Kristin.Seidelmann@gmx.de>
2026-07-06 13:01:23 +0200

    Add event listeners for each weapon img

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-let message:string = 'Hello World';
-console.log(message);
+import { Game } from "./game";
+import { Weapon } from "./types";
+import { UI } from "./ui";
+
+const game = new Game();
+const ui = new UI();
+
+const weaponElements =
+    document.querySelectorAll<HTMLImageElement>(".weapon-img");
+
+weaponElements.forEach((element) => {
+    element.addEventListener("click", () => {
+        const weapon: Weapon = element.id as Weapon;
+        const result = game.play(weapon);
+        
+        ui.showResult(result.result);
+        ui.renderScores(0, 0);
+    })
+})

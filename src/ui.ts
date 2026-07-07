bcb9a3e8b14cc3ec47c2b5c51786dc9e9514cdb7
Kristin Seidelmann <Kristin.Seidelmann@gmx.de>
2026-07-07 18:15:47 +0200

    Improve result messages + add logic for markers & disable/enable weapons

--- src/ui.ts
+++ src/ui.ts
@@ -1,19 +1,35 @@
+import { GameResult, Weapon } from "./types";
+
+const resultMessages = {
+    win: "You won!",
+    lose: "You lost!",
+    draw: "It's a draw!"
+};
+
 export class UI {
     private userScoreElement: HTMLSpanElement;
     private computerScoreElement: HTMLSpanElement;
-    private roundResultElement: HTMLDivElement;
+    private roundResultElement: HTMLParagraphElement;
     private overlayElement: HTMLDivElement;
     private winnerTextElement: HTMLHeadingElement;
     private restartButtonElement: HTMLButtonElement;
+    private userMarkerElements: NodeListOf<HTMLSpanElement>;
+    private computerMarkerElements: NodeListOf<HTMLSpanElement>;
+    private weaponElements: NodeListOf<HTMLImageElement>;
+    private infoTextElement: HTMLParagraphElement;
 
     constructor() {
         //Get DOM elements
         this.userScoreElement = document.querySelector<HTMLSpanElement>("#user-score")!;
         this.computerScoreElement = document.querySelector<HTMLSpanElement>("#computer-score")!;
-        this.roundResultElement = document.querySelector<HTMLDivElement>("#round-result")!;
+        this.roundResultElement = document.querySelector<HTMLParagraphElement>("#round-result")!;
         this.overlayElement = document.querySelector<HTMLDivElement>("#game-over-overlay")!;
         this.winnerTextElement = document.querySelector<HTMLHeadingElement>("#winner-text")!;
         this.restartButtonElement = document.querySelector<HTMLButtonElement>("#restart-button")!;
+        this.userMarkerElements = document.querySelectorAll<HTMLSpanElement>(".user-marker")!;
+        this.computerMarkerElements = document.querySelectorAll<HTMLSpanElement>(".computer-marker")!;
+        this.weaponElements = document.querySelectorAll<HTMLImageElement>(".weapon-img")!;
+        this.infoTextElement = document.querySelector<HTMLParagraphElement>("#info-text")!;
     }
 
     renderScores(userScore: number, computerScore: number): void {
@@ -21,17 +37,22 @@ export class UI {
         this.computerScoreElement.textContent = String(computerScore);
     }
 
-    showResult(result: string): void {
-        this.roundResultElement.textContent = result;
+    showResult(result: GameResult["result"]): void {
+        console.log(`${resultMessages[result]}`);
+        this.roundResultElement.textContent = resultMessages[result];
     }
+
+    clearResult(): void {
+    this.roundResultElement.textContent = "";
+}
 
     resetUI(): void {
         this.renderScores(0, 0);
-        this.showResult("");
+        this.clearResult();
     }
 
     showGameOver(winner: string): void {
-        const message: string = winner == "user" ? "You won!" : "Computer won!";
+        const message: string = winner == "user" ? "Congratulations! You won!" : "Oh no, the computer won!";
         this.winnerTextElement.textContent = message;
         this.showOverlay();
     }
@@ -47,5 +68,41 @@ export class UI {
     // Receives a callback function from outside and executes it when the restart button is clicked
     onRestart(callback: () => void): void {
         this.restartButtonElement.addEventListener("click", callback);
+    }
+
+    clearMarkers(): void {
+        this.userMarkerElements.forEach((marker) => {
+            marker.classList.remove("visible");
+        });
+        this.computerMarkerElements.forEach((marker) => {
+            marker.classList.remove("visible");
+        })
+    }
+
+    showUserMarker(weapon: Weapon): void {
+        console.log(`#${weapon} + div .user-marker`);
+        const marker = document.querySelector(`#${weapon} + div .user-marker`);
+        marker?.classList.add("visible");
+    }
+
+    showComputerMarker(weapon: Weapon): void {
+        const marker = document.querySelector(`#${weapon} + div .computer-marker`);
+        marker?.classList.add("visible");
+    }
+
+    disableWeapons(): void {
+        this.weaponElements.forEach((weapon) => {
+            weapon.classList.add("disabled");
+        })
+    }
+
+    enableWeapons(): void {
+        this.weaponElements.forEach((weapon) => {
+            weapon.classList.remove("disabled");
+        })
+    }
+
+    setInfoText(text: string): void {
+        this.infoTextElement.textContent = text;
     }
 }
